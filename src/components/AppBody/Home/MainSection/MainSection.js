ef0a75ea9593f299b21df5be3d39d7008f36f338
Ass4sin <tudorandrei2010@yahoo.com>
2024-07-26 15:38:20 +0200

    feat: Add FontAwesome icons and update home page styles

--- src/components/AppBody/Home/MainSection/MainSection.js
+++ src/components/AppBody/Home/MainSection/MainSection.js
@@ -1,16 +1,42 @@
 import "./MainSection.css";
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'
+
+const API_KEY = "8c8ba6c5a4d6a0980c664822f1721e07";
+const API_ID = "fc0e8ed5";
 
 export default function MainSection() {
-    return(
-        <main>
-            <div>
-                {/* we ill add the a carousel of images from the EDAMAM API */}
-            </div>
-        </main>
-    )
+
+    const [query, setQuery] = useState('rice');
+    const [recipeImages, setRecipeImage] = useState([]);
+
+    useEffect(() => {
+        // Fetch the data from the API
+        fetch(`https://api.edamam.com/api/recipes/v2?type=public&app_id=${API_ID}&app_key=${API_KEY}&q=${query}`)
+            .then(res => res.json())
+            .then(data => {
+                // Extract image URLs from the results
+                const images = data.hits.map(hit => hit.recipe.image);
+                setRecipeImage(images);
+                console.log(images);
+            });
+    }, [query]);
+
+    return (
+        <>
+            <main>
+                <div className="main-container">
+                    <div className="image-container">
+                        <FontAwesomeIcon icon={faArrowLeft} id="arrow-left" />
+                        {recipeImages.length > 0 && <img src={recipeImages[0]} alt="Recipe" />}
+                        <FontAwesomeIcon icon={faArrowRight} id="arrow-right"/>
+                    </div>
+                </div>
+            </main>
+        </>
+    );
 }
 
-// this function will serve to call the api and get the images
-
-export {MainSection};
+// This function will serve to call the API and get the images
+export { MainSection };
