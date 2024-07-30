37426878958191bb13ea7f884f6fce7efe403928
Ass4sin <tudorandrei2010@yahoo.com>
2024-07-30 22:18:28 +0200

    wrote the idea for the recipe page

--- src/pages/Recipes/Recipes.js
+++ src/pages/Recipes/Recipes.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./Recipes.module.css";
 
+//on this page you should be able to search for a recipe using a search bar. then when you click on an image you can see the ingredients needed to make said recipe
 const Recipes = () => {
     return (
         <div>
-            <h1>Recipes Page</h1>
+            recipes
         </div>
     );
 }
